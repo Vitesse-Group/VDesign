65f9d7bc87bd71ebf1b0a3dc7eab6581508be15f
Harishsoma <harishkumar.soma@vitesse-group.com>
2020-02-28 07:53:33 -0600

    updated gitignore file

--- src/main/java/com/vitesse/controller/HomeController.java
+++ src/main/java/com/vitesse/controller/HomeController.java
@@ -5,6 +5,7 @@ import com.vitesse.repository.AppRepository;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +22,8 @@ public class HomeController {
 	}
     
     @RequestMapping(value="/Names", method=RequestMethod.GET)
-	public List<String> Name() {
-    	System.out.println("Test Controller");
+	public List<String> Name(@RequestHeader("x-location")String location) {
+    	System.out.println("Hi" + location);
 		return appRepository.Names();
 	}
 }
